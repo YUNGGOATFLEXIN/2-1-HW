ae24029feb967b0cec36d35acfcf1e1c8849b22d
YUNGGOATFLEXIN <boosha@mail.ru>
2022-09-13 11:41:52 +1000

    выполнена домашняя работа №2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-let user = ('user name');
-    user = 5;
+// let user = ('user name');
+//     user = 5;
 
-console.log(user);
+// console.log(user);
 
+
+
+// Домашнее задание №2
+
+const newArray = ['one', 2, true, 'four', 5];
+console.log(newArray[2]);
+
+const newObj = {
+    color: '#fff',
+    model: 'iphone',
+    price: 50000,
+};
+
+const key = 'color';
+console.log(newObj[key]);
+
+const keyTwo = 'model';
+console.log(newObj[keyTwo]);
+
+const keyThree = 'price';
+console.log(newObj[keyThree]);
