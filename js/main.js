642f0b8ae2c3a41913aa2c230fd9c3be8f62f127
YUNGGOATFLEXIN <boosha@mail.ru>
2022-09-18 23:36:25 +1000

    выполнил 3 задание

--- js/main.js
+++ js/main.js
@@ -7,20 +7,55 @@
 
 // Домашнее задание №2
 
-const newArray = ['one', 2, true, 'four', 5];
-console.log(newArray[2]);
+// const newArray = ['one', 2, true, 'four', 5];
+// console.log(newArray[2]);
 
-const newObj = {
-    color: '#fff',
-    model: 'iphone',
-    price: 50000,
+// const newObj = {
+//     color: '#fff',
+//     model: 'iphone',
+//     price: 50000,
+// };
+
+// console.log(newObj.color);
+// console.log(newObj.model);
+// console.log(newObj.price);
+
+
+
+
+// Домашнее задание №3
+
+const arr = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 };
 
-const key = 'color';
-console.log(newObj[key]);
+const sayHello = () => {
+    let name = prompt('Как тебя зовут?');
+    alert(`Привет, ${name}`)
+};
 
-const keyTwo = 'model';
-console.log(newObj[keyTwo]);
+sayHello();
 
-const keyThree = 'price';
-console.log(newObj[keyThree]);
+const getAge = () => {
+    let age = prompt('Сколько вам лет?', '');
+
+    age < 5 ? alert('Вы младенец') :
+    age > 5 && age < 10 ? alert('Вы ребенок') :
+    age > 10 && age < 18 ? alert('Вы подросток') :
+    age > 18 && age < 65 ? alert('Вы взрослый') :
+    alert('Вы старик');
+};
+
+getAge();
+
+const getNum = () => {
+    let num = prompt('Назовите любое число от 3 до 5', '');
+
+    num % 3 == 0 && num % 5 == 0 ? alert('FizzBuzz') :
+    num % 3 == 0 ? alert('Fizz') :
+    num % 5 == 0 ? alert('Buzz') :
+    alert('Выберите теперь 3 или 5 или и 3 и 5 одновременно');
+};
+
+getNum();
