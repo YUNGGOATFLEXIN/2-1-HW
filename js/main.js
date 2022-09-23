cfd97fb9511acfd0175463c9aa6efacb147580c3
YUNGGOATFLEXIN <boosha@mail.ru>
2022-09-23 16:04:46 +1000

    Выполнил задание №4

--- js/main.js
+++ js/main.js
@@ -59,3 +59,27 @@
 // };
 
 // getNum();
+
+
+
+
+// Домашнее задание №4
+
+const button = document.querySelector('.form__btn');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const password = document.getElementById('password');
+    const login = document.getElementById('login');
+
+    if (password.value + login.value == '') {
+        alert('Введите ваши данные для входа');
+    } else if (login.value == '') {
+        alert('Введите ваш логин');
+    } else if (password.value == '') {
+        alert('Введите ваш пароль')
+    } else if (login.value && password.value != '') {
+        alert(`Ваш логин: ${login.value}. Ваш пароль: ${password.value}.`)
+    };
+});
